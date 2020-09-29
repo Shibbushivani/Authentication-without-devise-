9d030558e0aa5ec6e519150507d522cfcb4c678c
Shibbushivani <shivani.gupta432@gmail.com>
2020-09-29 23:50:39 +0530

    add rolify gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      user.add_role :admin
       session[:user_id] = user.id
       redirect_to '/'
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  rolify
   has_secure_password
+  after_create :assign_default_role
+  def assign_default_role
+    self.add_role(:admin) if self.roles.blank?
+  end
 end
