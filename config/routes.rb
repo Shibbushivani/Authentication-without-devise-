910097c3dcd109b7316c56af98ad97f6a3975f97
Shibbushivani <shivani.gupta432@gmail.com>
2020-09-29 23:34:23 +0530

    add commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :welcomes
+  root 'welcomes#index'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login'  => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
